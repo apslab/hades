26fef15e824f8f4f06fa4dcc81a662fd6b00c960
luis petek <lmpetek@gmail.com>
2011-05-24 16:19:11 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hades::Application.config.secret_token = 'c669b5eae8b9a640f477293e40aa4b6bafed98c5cd6f48b1441ab507aed0a6e475124d105ee78bc4039379bc68e9111367a50a980e76019038b982c5bec5dba0'
